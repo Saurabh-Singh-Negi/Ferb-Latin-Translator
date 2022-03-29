46cfea1f5d0ec3013d376af8a4491039154a7a7a
Saurabh-Singh-Negi <saurabhnegi0007@gmail.com>
2022-03-29 23:43:36 +0530

    added API & button click functionality

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ var inputText = document.querySelector("#txt-area");
 var btnTranslate = document.querySelector("#btn-translate");
 var outputText = document.querySelector("#txt-output");
 
-var url = "https://api.funtranslations.com/translate/minion.json";
+var url = "https://api.funtranslations.com/translate/ferb-latin.json";
 
 function getUrl(text) {
     return url + "?" + "text=" + text; 
 }
+
+
+function clickHandler() {
+    var finalUrl = getUrl(inputText.value);
+    fetch(finalUrl)
+    .then(response => response.json())
+    .then(json => {
+        var msgOutput = json.contents.translated;
+        outputText.innerText = msgOutput;
+    }) 
+}
+
+btnTranslate.addEventListener("click", clickHandler);
